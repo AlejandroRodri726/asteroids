0e1ce91f5b982e54a86671a3277aac1a586ef061
alexdro <alejandrorpga@gmail.com>
2025-12-11 13:03:47 -0600

    C3: Added Player and Partial Movement

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from logger import *
+from player import *
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -8,8 +9,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clk = pygame.time.Clock()
-    dt = 0.0
+    dt = 0
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
@@ -17,8 +19,10 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
-        dt = clk.tick(60) / 1000.0
+        dt = clk.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
