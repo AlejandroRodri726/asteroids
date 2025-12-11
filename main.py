9a701a34a547d5cb598e3956039bb2ca6406fc33
alexdro <alejandrorpga@gmail.com>
2025-12-11 13:15:09 -0600

    Player implemented

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ def main():
     clk = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
@@ -19,9 +23,11 @@ def main():
                 return
 
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        updatable.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
+        
         dt = clk.tick(60) / 1000
 
 if __name__ == "__main__":
